9ce3e772546f94d8ecc0e9696c7d8c7aa94b58d7
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2023-10-28 22:22:15 +0530

    Update 1.SinglyLinkedList.py

--- LinkedList/1.SinglyLinkedList.py
+++ LinkedList/1.SinglyLinkedList.py
@@ -1,12 +1,19 @@
-#--------------Implimentation of Linked List--------------------#
+#-----------------------------------Implimentation of Linked List-------------------------------------#
 class Node:
 
   def __init__(self, data, next=None):
     self.data = data
     self.next = next
-#----------------------Traversal in a linked list------------------------#
+#----------------------------------Traversal in a linked list---------------------------------#
+#------------------------Method.1---------------------------#
  def Traversal(head):
    curr=head
    while(curr):
      print(curr.val)
      curr=curr.next
+#----------------------Method.2---------------------------#
+def Streversing(head):
+  if head==Null:
+    return
+  print(head.val)
+  Straversing(head.next)
