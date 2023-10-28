3a0dc5e1c8101d344c4ae8b3e5dffae3dd4b77ca
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2023-10-28 22:13:15 +0530

    Update 1.SinglyLinkedList.py

--- LinkedList/1.SinglyLinkedList.py
+++ LinkedList/1.SinglyLinkedList.py
@@ -1,1 +1,8 @@
+#--------------Implimentation of Linked List--------------------#
+class Node:
 
+  def __init__(self, data, next=None):
+    self.data = data
+    self.next = next
+#----------------------Traversal in a linked list------------------------#
+ 
