1f87b974c88d7c053b796b8551dfc21bbe3797de
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-05-20 22:24:38 +0530

    Update Recursion.py

--- 01_Recursion/Recursion.py
+++ 01_Recursion/Recursion.py
@@ -131,51 +131,181 @@ print(fact(n))
 """
 
 # Ex :- 02 = nth Fibonacci number where n >= 0
-# def fib(n):
-#     if n <= 1:
-#         return n
-#     return fib(n - 1) + fib(n-2)
-#
-#
-# n = int(input())
-# print(fib(n))
+"""def fib(n):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n-2)
 
 
-# def fun(s):
-#     l = list(s)
-#     T = l.count("A")
-#     print(T)
-#     for i in range(len(s) - 2):
-#         if T < 2 and (l[i] == "L" and l[i + 1] == "L" and l[i + 2] == "L"):
-#             return True
-#         else:
-#             return False
-#
-#
-# s = input()
-# print(fun(s))
+n = int(input())
+print(fib(n))"""
 
+
+"""def fun(s):
+    l = list(s)
+    T = l.count("A")
+    print(T)
+    for i in range(len(s) - 2):
+        if T < 2 and (l[i] == "L" and l[i + 1] == "L" and l[i + 2] == "L"):
+            return True
+        else:
+            return False
+
+
+s = input()
+print(fun(s))
 
 # x = 1 / 2 + 3 // 3 + 4 ** 2
 # print(x)
-
+"""
 
 # ------------------ Trapping rain water -------------------#
-# def getWater(arr, n):
-#     res = 0
-#     for i in range(1, n-1):
-#         left_max = arr[i]
-#         for j in range(0, i):
-#             left_max = max(left_max, arr[j])
-#         right_max = arr[i]
-#         print(right_max)
-#         for j in range(i+1, n):
-#             right_max = max(right_max, arr[j])
-#         res = res + (min(left_max, right_max) - arr[i])
-#
-#     return res
-#
-#
-# arr = list(map(int, input().split()))
-# n = int(input())
-# print(getWater(arr, n))
+"""def getWater(arr, n):
+    res = 0
+    for i in range(1, n-1):
+        left_max = arr[i]
+        for j in range(0, i):
+            left_max = max(left_max, arr[j])
+        right_max = arr[i]
+        print(right_max)
+        for j in range(i+1, n):
+            right_max = max(right_max, arr[j])
+        res = res + (min(left_max, right_max) - arr[i])
+
+    return res
+
+
+arr = list(map(int, input().split()))
+n = int(input())
+print(getWater(arr, n))
+"""
+
+# ------------------- Sum of All Natural numbers using recursion ---------------- #
+"""def getSum(num):
+    if(num==0):
+        return 0
+    return getSum(num-1) + num
+
+num= int(input())
+print(getSum(num))
+"""
+
+# -------------------- Pallindrome check using Recursion ---------------#
+"""def isPallindrome(str, start, end):
+    if (start >= end):
+        return True
+    return (str[start] == str[end])
+    isPallindrome(str, start+1, end-1)
+
+
+str = input()
+start= int(input())
+end = int(input())
+print(isPallindrome(str, start, end))
+"""
+
+# ------------- Sum of Digits using Recursion ------------#
+"""def getSum(num):
+    if (num==0):
+        return 0
+    else:
+        return getSum(num//10) + num % 10
+
+
+num = int(input())
+print(getSum(num))
+"""
+
+#Iterative Solution
+"""def getSum(n):
+    res =0
+    while(n>=0):
+        res = res+n%10
+        n=n/10
+    return res
+
+n = int(input())
+print(getSum(n))"""
+
+# -------------Rope Cutting problem -------------#
+"""def maxPieces(n, a, b, c):
+    if (n==0):
+        return 0
+    if(n<0):
+        return -1
+    res = max(maxPieces(n-a, a, b, c), maxPieces(n-b, a, b, c), maxPieces(n-c, a, b, c))
+    if (res == -1):
+        return -1
+    return res+1
+
+n, a, b, c = map(int,input().split())
+print(maxPieces(n, a, b, c))
+"""
+
+# ------------Generate Subsets ---------------#
+"""def subsets(str, curr, i):
+    if(i==len(str)):
+        print(str)
+        return
+    subsets(str, curr, i+1)
+    subsets(str, curr+str[i], i+1)
+
+str = input()
+curr = ""
+i= int(input())
+print(subsets(str, curr, i))
+"""
+
+# ----------------- Tower of Hanoi --------------#
+"""def TOH(n, A, B, C):
+    if(n==1):
+        print("Move 1 from", A, "to", C)
+        return
+    TOH(n-1, A, C, B)
+    print("Move", n, "from", A, "to", C)
+    TOH(n-1, B, A, C)
+
+
+n= int(input())
+A, B , C= map(int,input().split())
+print(TOH(n, A, B, C))
+"""
+
+# --------------- Josephus Problem -------------------#
+# ------------------- if starting index is from 0 --------------------#
+"""def JOS(n, K):
+    if(n==1):
+        return 0
+    else:
+        return (JOS(n-1, K) + K)%n
+
+
+n, K= map(int,input().split())
+print(JOS(n, K))
+ """
+
+# ---------------- If starting index is from 1 -------------#
+"""def JOS(n, k):
+    if (n==1):
+        return 0
+    else:
+        return (JOS(n-1, k)+k)%n
+def myJOS(n, k):
+    return JOS(n, k) + 1
+
+
+n, k= map(int,input().split())
+print(myJOS(n, k))
+"""
+
+
+# ---------------- Subset Sum Problem --------------------#
+"""def CountSubsets(arr, n, sum1):
+    if (n==0):
+        return 1 if sum1 ==0 else 0
+    return CountSubsets(arr, n-1, sum1) + CountSubsets(arr, n-1, sum1-arr[n-1])
+
+arr = list(map(int,input().split()))
+n, sum1 = map(int,input().split())
+print(CountSubsets(arr, n, sum1))
+"""
