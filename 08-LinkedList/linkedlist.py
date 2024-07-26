b4d21eeea7f5bc58797b0cb2c061d4b8e1dc2a74
Akash Singh <112417241+akash-abesec@users.noreply.github.com>
2024-07-26 22:10:05 +0530

    Update linkedlist.py

--- 08-LinkedList/linkedlist.py
+++ 08-LinkedList/linkedlist.py
@@ -266,7 +266,7 @@ def insertatend(curr,data):
     return curr
 
 
-#-------------------------- Deletion in Doubly Linked List ------------------------------------
+#-------------------------- Deletion in Doubly Linked List ------------------------------------#
 
 
 def delatbeg(curr):
@@ -287,7 +287,7 @@ def delatlast(curr):
     temp.next=None
     return curr
 
-#----------------------Reverse in Doubly Linked List----------------------------------
+#----------------------Reverse in Doubly Linked List----------------------------------#
 
 def reverse(curr):
     #Reversing by iterative Method
@@ -301,7 +301,7 @@ def reverse(curr):
         temp=temp.prev
     return pre
 
-#----------------------------------- Implementation of Single Circular LinkedList ----------------------------------
+#----------------------------------- Implementation of Single Circular LinkedList ----------------------------------#
 
 class Node:
     def __init__(self,data):
@@ -309,7 +309,7 @@ class Node:
         self.next=None
 
 
-#------------------------------- Traversal of Circular linked list ----------------------------------------------
+#------------------------------- Traversal of Circular linked list ----------------------------------------------#
 
 def traversal(curr):
     #Iterative traveral
@@ -322,7 +322,7 @@ def traversal(curr):
         temp=temp.next
 
 
-#----------------------------Insertion in Circular Linked List -------------------------------------
+#----------------------------Insertion in Circular Linked List -------------------------------------#
 
 def insertatbeg(curr,data):
     #Insert at beginning
@@ -347,7 +347,7 @@ def insertatend(curr,data):
     return curr.next
 
 
-#-------------------------------------- Deletion in a Circular LinkedList ------------------------------------
+#-------------------------------------- Deletion in a Circular LinkedList ------------------------------------#
 
 def delatbeg(curr):
     #Delete First Node
@@ -384,7 +384,7 @@ def deletekthnode(curr,pos):
     return curr
 
 
-#--------------------------------- Implementation of Doubly Circular LinkedList ------------------------------------------
+#--------------------------------- Implementation of Doubly Circular LinkedList ------------------------------------------#
 
 class DNode:
     def __init__(self,data):
@@ -416,6 +416,191 @@ n3.next=head
 # n3.next=head
 
 
+# --------------------------------- Sorted Insert in a Singly LinkedList -------------------------------------#
+
+def sortedinsert(curr,data):
+    newNode=Node(data)
+    if curr==None:
+        return newNode
+    temp=curr
+    while True:
+        if temp.val>=data:
+            newNode.next=temp.next
+            temp.next=newNode
+            newNode.val,temp.val=temp.val,newNode.val
+            return curr
+        if temp.next==None:
+            break
+        temp=temp.next
+    temp.next=newNode
+    return curr
+
+
+# ---------------------------- Middle of a LinkedList ------------------------------------#
+
+#In case of even node fast will be at None and in case of odd node fast.next will be none
+def middleofll(curr):
+    if curr==None:
+        return
+    elif curr.next==None:
+        return curr.val
+    fast=curr
+    slow=curr
+    while fast!=None and fast.next!=None:
+        slow=slow.next
+        fast=fast.next.next
+    return slow.val
+
+
+#   ---------------------- Nth Node From End of a LinkedList -------------------------------#
+# T-O(n)
+# s-O(1)
+
+def nthNodefromend(curr,pos):
+    if curr==None:return
+    first=curr
+    for i in range(pos):
+        if first==None:return
+        first=first.next
+    second=curr
+    while first!=None:
+        second=second.next
+        first=first.next
+    return second.val
+
+
+# ------------------------- Remove Duplicate in Sorted LinkedList -------------------------------------# 
+
+def removeDuplicateSLL(curr):
+    if curr==None or curr.next==None:
+        return curr
+    temp=curr
+    while temp.next!=None and temp!=None:
+        if temp.val==temp.next.val:
+            temp.next=temp.next.next
+        else:
+            temp=temp.next
+    return curr
+
+
+# ------------------------------ Detect Loop in LinkedList(Floyd Cycle Detection) -----------------------------------#
+
+def isloop(curr):
+    #Complacity O(length of linked list)
+    fast=curr
+    slow=curr
+    while fast!=None and fast.next!=None:
+        fast=fast.next.next
+        slow=slow.next
+        if slow==fast:return True
+    return False
+
+
+# ------------------------------- Remove loop of a linked list -------------------------------- #
+
+def removeloop(curr):
+    fast=curr
+    slow=curr
+    while fast!=None and fast.next!=None:
+        fast=fast.next.next
+        slow=slow.next
+        if fast==slow:
+            break
+    if fast!=slow:return curr
+    fast=curr
+    while fast.next!=slow.next:
+        fast=fast.next
+        slow=slow.next
+    slow.next=None
+    return curr
+
+
+# ------------------------ Segregate Even odd nodes ----------------------------------------#
+
+def SegregateEvenOdd(curr):
+    if curr==None:
+        return curr
+    estart,eend=None,None
+    ostart,oend=None,None
+    temp=curr
+    while temp!=None:
+        if temp.val%2==0:
+            if estart==None:
+                estart=temp
+                eend=temp
+            else:
+                eend.next=temp
+                eend=temp
+        else:
+            if ostart==None:
+                ostart=temp
+                oend=temp
+            else:
+                oend.next=temp
+                oend=temp
+        temp=temp.next
+    if estart==None:return ostart
+    elif ostart==None:return estart
+    else:
+        eend.next=ostart
+        oend.next=None
+        return estart
+
+
+# ------------------------------ Intersection of two Linked List ------------------------------------#
+
+def intersection(curr1,curr2):
+    c1,c2=0,0
+    temp1,temp2=curr1,curr2
+    while temp1!=None:
+        temp1=temp1.next
+        c1+=1
+    while temp2!=None:
+        temp2=temp2.next
+        c2+=1
+    diff=abs(c1-c2)
+    if c1>=c2:
+        for i in range(diff):
+            curr1=curr1.next
+    else:
+        for i in range(diff):
+            curr1=curr1.next
+    while curr1!=curr2:
+        curr1=curr1.next
+        curr2=curr2.next
+    print(curr1.val)
+
+
+# ------------------------- Merge Two Sorted LinkedList ---------------------------------#
+
+def mergeSorted(list1,list2):
+    if list1==None:
+        return list2
+    if list2==None:
+        return list1
+    head,ptr=None,None
+    while list1!=None and list2!=None:
+        if(list1.val>=list2.val):
+            if(head==None):
+                head=list2
+                ptr=list2
+            else:
+                ptr.next=list2
+                ptr=list2
+            list2=list2.next
+        else:
+            if(head==None):
+                head=list1
+                ptr=list1
+            else:
+                ptr.next=list1
+                ptr=list1
+            list1=list1.next
+    if(list1==None):
+        ptr.next=list2
+    else:
+        ptr.next=list1
+    return head
 
 
 
